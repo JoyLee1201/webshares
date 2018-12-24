cfc8807065f3bb5835710582b4930fb1d713427f
橘子 <893614438@qq.com>
2018-12-24 20:52:39 +0800

    V1.0
    V1.0

---
+++ files/合同管理/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A],B,_(C,D,E,F,G,H,I,_(),J,_(K,L,M,N,O,_(P,Q,R,S),T,null,U,N,V,N,W,X,Y,null,Z,ba,bb,bc,bd,be,bf,ba),bg,_(),bh,_(),bi,_(bj,[])),bk,_(),bl,_());}; 
+var b="url",c="合同管理.html",d="generationDate",e=new Date(1545655862150.68),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="UserName",l="CompanyName",m="InvestmentRound",n="InvestmentValue",o="RegisterCapital",p="NumofStocks",q="Rate1",r="Rate2",s="Rate3",t="Plan1",u="Rate4",v="RateNew1",w="RateNew2",x="Account",y="Password",z="NameofPlan",A="IfContract",B="page",C="packageId",D="839582112d5e4c709bf116dd32ff39f9",E="type",F="Axure:Page",G="name",H="合同管理",I="notes",J="style",K="baseStyle",L="627587b6038d43cca051c114ac41ad32",M="pageAlignment",N="near",O="fill",P="fillType",Q="solid",R="color",S=0xFFFFFFFF,T="image",U="imageHorizontalAlignment",V="imageVerticalAlignment",W="imageRepeat",X="auto",Y="favicon",Z="sketchFactor",ba="0",bb="colorStyle",bc="appliedColor",bd="fontName",be="Applied Font",bf="borderWidth",bg="adaptiveStyles",bh="interactionMap",bi="diagram",bj="objects",bk="masters",bl="objectPaths";
+return _creator();
+})());
